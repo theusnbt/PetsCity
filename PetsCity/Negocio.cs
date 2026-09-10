eaf41e9bde37064d9a046caf63a7686425823938
unknown <2amod@e041edu.local>
2026-09-10 16:58:57 -0300

    feat: configuração da classe (Matheus)

--- PetsCity/Negocio.cs
+++ PetsCity/Negocio.cs
@@ -8,5 +8,28 @@ namespace PetsCity
 {
     internal class Negocio
     {
+        public string NomePet {  get; set; }
+        public double PrecoServ { get; set; }
+        public int Quantidade {  get; set; }
+        public int IdadeAnimal {  get; set; }
+
+        public double CalcularDesconto()
+        {
+            double valorTotal = PrecoServ * Quantidade;
+
+            if(Quantidade > 3)
+            {
+                return valorTotal * 0.20;
+            }
+
+            return 0.0;
+        }
+        public double CalcularValorFinal()
+        {
+            double valorBruto = PrecoServ * Quantidade;
+            double desconto = CalcularDesconto();
+            return valorBruto = desconto;
+        }
+
     }
 }
